a31851ac1f82dc8ffc9a5307cbbbcd77005381f8
yohanessahrul <yohanessahrul92@gmail.com>
2018-03-16 00:13:59 +0700

    change oop concept

--- models/modelHospital.js
+++ models/modelHospital.js
@@ -1,4 +1,8 @@
 const fs = require('fs')
+const data = fs.readFileSync('./employee.json', 'utf8');
+
+const Employee = require('../models/modelEmployee')
+
 
 class Hospital {
   constructor(name, location, employees, patients) {
@@ -9,39 +13,35 @@ class Hospital {
   }
 
   static listEmployee(callback){
-    let data = fs.readFileSync('./employee.json', 'utf8');
     let dataEmploye = JSON.parse(data)
 
     callback(dataEmploye)
   }
 
   static registerEmployee(username, password, role, callback){
-    let data = fs.readFileSync('./employee.json', 'utf8');
     let dataEmploye = JSON.parse(data)
+    
+    let newEMploye = new Employee('', role, username, password)
 
-    let obj = {
-      username: username,
-      password: password,
-      role: role
-    };
-    dataEmploye.push(obj)
+    dataEmploye.push(newEMploye)
 
     let newData = JSON.stringify(dataEmploye, null, 2)
 
     fs.writeFileSync('./employee.json', newData)
 
-    callback(obj, dataEmploye.length)
+    callback(newEMploye, dataEmploye.length)
   }
 
   static login(username, password, callback){
-    let data = fs.readFileSync('./employee.json', 'utf8');
+    const data = fs.readFileSync('./employee.json', 'utf8');
     let dataEmploye = JSON.parse(data)
     let statusLogin;
     for(let i=0; i<dataEmploye.length; i++){
       if(dataEmploye[i].username == username && dataEmploye[i].password == password){
         return callback(true, dataEmploye[i].username)
+      } else {
+        return callback(false)
       }
-      return callback(false)
     }
   }
 }
